b8c01539aadb8b9e5a8ee192d847befc0470ec17
BuildTools <unconfigured@null.spigotmc.org>
2019-01-15 13:20:23 -0500

    Random maze creation algorithm. Random values for the middle

---
+++ venv/Random.py
@@ -0,0 +1,15 @@
+import random
+
+def create(size, start, end):
+    middle = [[0 for i in range(size[1])] for i in range(size[0])]
+    for y in range(0, size[1]-2):
+        for x in range(0, size[0]):
+            if x == 0 or x == size[0]-1:
+                middle[y][x] = 1
+            elif x == start and y == 0:
+                middle[y][x] = 0
+            if y == size[1]-3 and x == end:
+                middle[y][x] = 0
+            else:
+                middle[y][x] = random.randrange(2)
+    return middle
