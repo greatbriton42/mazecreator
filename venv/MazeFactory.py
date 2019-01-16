f45f02cff8165cffbeabda4b85a46d4b23e7d7fb
BuildTools <unconfigured@null.spigotmc.org>
2019-01-16 16:32:28 -0500

    Added support for rightpriority algorithm

--- venv/MazeFactory.py
+++ venv/MazeFactory.py
@@ -1,9 +1,12 @@
-class mazefactory:
+class MazeFactory:
     def __init__(self):
         self.Default = "random"
-        self.Choices = ["random"]
+        self.Choices = ["random", "rightpriority"]
 
     def importmaze(self, algorithm):
         if algorithm == "random":
             import Random
             return ["Random values for the maze", Random.create]
+        elif algorithm == "rightpriority":
+            import RightPriority
+            return ["Prioritorize going right", RightPriority.create]
